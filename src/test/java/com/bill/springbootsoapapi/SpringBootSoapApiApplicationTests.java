5f345b1b9e88e763b124d71f1e0199550bdeeb40
bill.lin <zzx123bill@gmail.com>
2024-09-19 22:01:36 +0800

    setup project

---
+++ src/test/java/com/bill/springbootsoapapi/SpringBootSoapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.springbootsoapapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
